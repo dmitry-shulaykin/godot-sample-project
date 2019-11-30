c1ccc87d31505b6c11492dfe23043bd547e29e45
zyec32 <zyec32@gmail.com>
2019-11-30 09:41:05 +0300

    Someer

--- hakvelon2019/scripts/person.gd
+++ hakvelon2019/scripts/person.gd
@@ -20,11 +20,14 @@ func _ready():
 
 func _physics_process(delta):
 	if path_ind < path.size():
-        var move_vec = (path[path_ind] - global_transform.origin)
-        if move_vec.length() < 0.1:
-            path_ind += 1
-        else:
-            move_and_slide(move_vec.normalized() * move_speed, Vector3(0, 1, 0))
+		var move_vec = (path[path_ind] - global_transform.origin)
+		
+		
+		if move_vec.length() < 0.1:
+			look_at(path[path_ind],Vector3(0,1,0))
+			path_ind += 1
+		else:
+			move_and_slide(move_vec.normalized() * move_speed, Vector3(0, 1, 0))
  
 func move_to(target_pos):
     path = nav.get_simple_path(global_transform.origin, target_pos)
