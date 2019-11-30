c68812305fe55a310d62788e887a5ae515603579
gollogov98 <gollogov98@gmail.com>
2019-11-30 06:57:30 +0300

    added request to movedoor

--- Camera recognition/doorcam/move.py
+++ Camera recognition/doorcam/move.py
@@ -1,6 +1,7 @@
 import cv2
 import numpy as np
 import configRecogn as config
+import requests
 
 cap = cv2.VideoCapture("rtsp://" + config.RecognitionConfig['user'] + ':' + config.RecognitionConfig['password'] + '@' + config.RecognitionConfig['camera'])
 
@@ -26,56 +27,52 @@ yMin402 = 200
 yMax402 = frame_height - 500
 
 out = cv2.VideoWriter("output.avi", fourcc, 5.0, (frame_width,frame_height))
+pause = 0
 
 ret, frame1 = cap.read()
 ret, frame2 = cap.read()
 
 while cap.isOpened():
-    diff = cv2.absdiff(frame1, frame2)
-    gray = cv2.cvtColor(diff, cv2.COLOR_BGR2GRAY)
-    blur = cv2.GaussianBlur(gray, (5,5), 0)
-    _, thresh = cv2.threshold(blur, 20, 255, cv2.THRESH_BINARY)
-    dilated = cv2.dilate(thresh, None, iterations=3)
-    contours, _ = cv2.findContours(dilated, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    if pause == 0:
+        diff = cv2.absdiff(frame1, frame2)
+        gray = cv2.cvtColor(diff, cv2.COLOR_BGR2GRAY)
+        blur = cv2.GaussianBlur(gray, (5,5), 0)
+        _, thresh = cv2.threshold(blur, 20, 255, cv2.THRESH_BINARY)
+        dilated = cv2.dilate(thresh, None, iterations=3)
+        contours, _ = cv2.findContours(dilated, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
-    message = "Not found"
-    color = (255, 255, 255)
+        room = -1
 
-    for contour in contours:
-        (x, y, w, h) = cv2.boundingRect(contour)
+        for contour in contours:
+            (x, y, w, h) = cv2.boundingRect(contour)
 
-        message = "Not found"
-        color = (255, 255, 255)
+            room = -1
 
-        if w < 50 or h < 50:
-            continue
+            if w < 50 or h < 100:
+                continue
 
-        if y + h / 2 > yMin400 and y + h / 2 < yMax400 and x + w / 2 > xMin400 and x + w / 2 < xMax400:
-            message = "400"
-            color = (255, 0, 0)
-            break
+            if y + h / 2 > yMin400 and y + h / 2 < yMax400 and x + w / 2 > xMin400 and x + w / 2 < xMax400:
+                room = 400
+                break
 
-        if y + h / 2 > yMin401 and y + h / 2 < yMax401 and x + w / 2 > xMin401 and x + w / 2 < xMax401:
-            message = "401"
-            color = (0, 255, 0)
-            break
+            if y + h / 2 > yMin401 and y + h / 2 < yMax401 and x + w / 2 > xMin401 and x + w / 2 < xMax401:
+                room = 401
+                break
 
-        if y + h / 2 > yMin402 and y + h / 2 < yMax402 and x + w / 2 > xMin402 and x + w / 2 < xMax402:
-            message = "402"
-            color = (0, 0, 255)
-            break
+            if y + h / 2 > yMin402 and y + h / 2 < yMax402 and x + w / 2 > xMin402 and x + w / 2 < xMax402:
+                room = 402
+                break
 
-        if cv2.contourArea(contour) < 900:
-            continue
+            if cv2.contourArea(contour) < 900:
+                continue
 
-    if message != "Not found":
-        cv2.rectangle(frame1, (x, y), (x+w, y+h), color, 2)
-        cv2.putText(frame1, "Status: {} x: {} y: {} w: {} h: {}".format(message, x + w / 2, y + h / 2, w, h), (10, 20), cv2.FONT_HERSHEY_SIMPLEX,
-            1, (0, 0, 255), 3)
+        if room != -1:
+            req = {'camId': config.RecognitionConfig['camId'], 'roomId': room}
+            requests.post(config.RecognitionConfig['serverUrl'], data = req)
+            pause = 12
+    else: 
+        pause = pause - 1
 
-    image = cv2.resize(frame1, (frame_width,frame_height))
-    out.write(image)
-    cv2.imshow("feed", frame1) # [yMin:yMax, xMin:xMax]
     frame1 = frame2
     ret, frame2 = cap.read()
 
